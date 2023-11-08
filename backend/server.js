b144b3223cf5a346b35eeab95b85fb8e6967697c
Nguyễn Văn Vũ <85126015+vuktmt15@users.noreply.github.com>
2023-11-08 20:59:00 +0700

    [20205050] - [Connect MongoDB]

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
 app.get("/api/chat", (req, res) => {
   res.send("List Chats");
@@ -14,5 +16,5 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`.yellow.bold);
 });
