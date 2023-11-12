2e9955bb1480dd2aca2689e03c5dd4bf58574f10
Nguyễn Văn Vũ <85126015+vuktmt15@users.noreply.github.com>
2023-11-13 00:36:31 +0700

    [20204807] - [Building chat page UI]

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,15 +1,27 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import { Box } from "@chakra-ui/layout";
+import { useState } from "react";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
+import MyChats from "../components/MyChats";
 
-const ChatPage = () => {
-  const fetchChats = async () => {
-    const data = await axios.get("/api/chat");
-    console.log(data);
-  };
-  useEffect(() => {
-    fetchChats();
-  }, []);
-  return <div>Chatpage</div>;
+const Chatpage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats fetchAgain={fetchAgain} />}
+      </Box>
+    </div>
+  );
 };
 
-export default ChatPage;
+export default Chatpage;
